c0f4d5459d7cb3c4c99abef538ac0418972a0765
Tng_Dng <t.dng08@gmail.com>
2023-06-11 18:17:14 +0700

    cau truc project

---
+++ Travel_Project/app.js
@@ -0,0 +1,30 @@
+//fake data tour
+const fakeData = require("./fakedata/fakeToursdata.json");
+
+//Thêm thư viện
+const express = require("express");
+const ejs = require("ejs");
+const ejsLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+//template engine
+app.set("views", "./views/layouts");
+app.set("view engine", "ejs");
+app.use(ejsLayouts);
+
+//Static file
+app.use("/public", express.static(__dirname + "/public"));
+
+const PORT = 3000;
+
+
+//Get home page
+app.get("/", (req, res) => {
+  return res.render("../index",{title: 'Trang chủ' , tours:fakeData});
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
