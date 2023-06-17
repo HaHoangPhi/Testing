ec9f2ff9e3dad0de8a2e827eb930a314c69e341d
Tng_Dng <t.dng08@gmail.com>
2023-06-18 02:30:44 +0700

    post data to server and loop to render form

--- Travel_Project/app.js
+++ Travel_Project/app.js
@@ -5,6 +5,7 @@ const fakeData = require("./fakedata/fakeToursdata.json");
 const express = require("express");
 const ejs = require("ejs");
 const ejsLayouts = require("express-ejs-layouts");
+const nodeMailer = require("nodemailer");
 
 const app = express();
 
@@ -13,16 +14,27 @@ app.set("views", "./views/layouts");
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
 
+//
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //Static file
 app.use("/public", express.static(__dirname + "/public"));
+
 const PORT = 3000;
 
+//nodemailer
 
 //Get home page
 app.get("/", (req, res) => {
-  return res.render("../index",{title: 'Trang chủ' , tours:fakeData});
+  return res.render("../index", { title: "Trang chủ", tours: fakeData });
 });
 
+app.post("/",(req, res) => {
+  const hovaten = req.body;
+  console.log(hovaten);
+  res.redirect('/')
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
